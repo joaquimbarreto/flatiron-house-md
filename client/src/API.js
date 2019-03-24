da5b9eb66c3aca03acd311bb9a957c67265468c4
joaquimbarreto <joaquim@barreto.co>
2019-03-24 14:47:04 +0000

    API.js

--- client/src/API.js
+++ client/src/API.js
@@ -1,48 +1,50 @@
 class API {
+	static init() {
+		process.env.REACT_APP_STAGE === "dev"
+			? (this.base_URL = "http://localhost:3001")
+			: (this.base_URL = "https://flatiron-house-md.herokuapp.com");
+	}
 
-  static getSymptoms(bodypart) {
-   return fetch ('http://localhost:3001/api/v1/filtersymptoms/', {
-        method: 'POST',
-        headers: { 
-          'content-type': 'application/json', 
-          'Authorization': localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            category: bodypart
-          })
-    }).then(res => res.json())
-    }
+	static getSymptoms(bodypart) {
+		return fetch(this.base_URL + "/api/v1/filtersymptoms/", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				Authorization: localStorage.getItem("token")
+			},
+			body: JSON.stringify({
+				category: bodypart
+			})
+		}).then(res => res.json());
+	}
 
-    static getSymptomsOfIssue(issueID) {
-        return fetch ('http://localhost:3001/api/v1/getissuesymptoms/', {
-             method: 'POST',
-             headers: { 
-               'content-type': 'application/json', 
-               'Authorization': localStorage.getItem('token')
-             },
-             body: JSON.stringify({
-                 id: issueID
-               })
-         }).then(res => res.json())
-         }
+	static getSymptomsOfIssue(issueID) {
+		return fetch(this.base_URL + "/api/v1/getissuesymptoms/", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				Authorization: localStorage.getItem("token")
+			},
+			body: JSON.stringify({
+				id: issueID
+			})
+		}).then(res => res.json());
+	}
 
+	static getDiagnoses(idArray) {
+		return fetch(this.base_URL + "/api/v1/filterdiagnoses/", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				Authorization: localStorage.getItem("token")
+			},
+			body: JSON.stringify({
+				ids: idArray
+			})
+		}).then(res => res.json());
+	}
+}
 
+API.init();
 
-    static getDiagnoses(idArray) {
-        return fetch ('http://localhost:3001/api/v1/filterdiagnoses/', {
-             method: 'POST',
-             headers: { 
-               'content-type': 'application/json', 
-               'Authorization': localStorage.getItem('token')
-             },
-             body: JSON.stringify({
-                 ids: idArray
-               })
-         }).then(res => res.json())
-        }
-
- }
-
-
-export default API
-
+export default API;
